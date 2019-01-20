82f5decda132a2302c349cdba12b250730bdffda
Dean Milojevic <milojevicdean@gmail.com>
2019-01-20 21:54:43 +0100

    Being bored is a dangerous thing. Decided to rewrite the implementation of dispatcher to be a little bit more generic. Just for fun.

--- CQRS.Example/MyDispatcher.cs
+++ CQRS.Example/MyDispatcher.cs
@@ -1,20 +1,47 @@
-﻿namespace CQRS.Example
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CQRS.Example
 {
     public sealed class MyDispatcher : IDispatcher
     {
         private readonly IMailbox _mailbox;
         private readonly IReportCommandExecution _reportCommandExecution;
 
+        private readonly Dictionary<Type, Type> _commandToHandlerMapping;
+
         public MyDispatcher(IReportCommandExecution reportCommandExecution, IMailbox mailbox)
         {
             _reportCommandExecution = reportCommandExecution;
             _mailbox = mailbox;
+
+            _commandToHandlerMapping = new Dictionary<Type, Type>();
+            DiscoverCommandsAndHandlers();
+        }
+
+        private void DiscoverCommandsAndHandlers()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var types = assembly.GetTypes();
+            foreach (var command in types.Where(t => t.GetInterface("ICommand") != null))
+            {
+                var handler = types.First(t => t.Name == command.Name + "Handler");
+                _commandToHandlerMapping[command] = handler ?? throw new ArgumentNullException($"The {command} has no defined handler for it.");
+            }
         }
 
         public void Send(ICommand command)
         {
             _mailbox.Insert(command);
-            new MyFirstCommandHandler(_reportCommandExecution).Handle((MyFirstCommand)command);
+
+            var handler = _commandToHandlerMapping[command.GetType()];
+
+            var instance = (dynamic)Activator.CreateInstance(handler, _reportCommandExecution);
+
+            instance.Handle((dynamic)command);
         }
     }
 }
